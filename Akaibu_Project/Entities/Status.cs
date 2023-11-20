82800b111506706b53fe1951dcdfcda278e4a8a1
GryczonJ <92317770+GryczonJ@users.noreply.github.com>
2023-11-20 22:22:40 +0100

    Akaibu_Project/Entions/Status Status -> status

--- Akaibu_Project/Entities/Status.cs
+++ Akaibu_Project/Entities/Status.cs
@@ -5,6 +5,6 @@
         public int ID_USER { get; set; }
         public int ID_ANIME { get; set; }
         public int Last_Epizod { get; set; }
-        public string Status {  get; set; }
+        public string status {  get; set; }
     }
 }

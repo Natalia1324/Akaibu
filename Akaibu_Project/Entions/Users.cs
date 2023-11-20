e07653d94edbd29af50fc7b7fb3ee5bfe77202d8
GryczonJ <92317770+GryczonJ@users.noreply.github.com>
2023-11-20 21:15:22 +0100

    My fires class in dbAkaibu ;) Users: - ID_User - Nick - Email - Password - Ranks
    Change value in file: Propertis/launchSettings.json
    new: "sslPort": 0
    old: "sslPort": null

---
+++ Akaibu_Project/Entions/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace Akaibu_Project.Entions
+{
+    public class Users
+    {
+        public int ID_User { get; set; }
+        public string Nick { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Ranks { get; set; }
+
+        // Properties
+    }
+}

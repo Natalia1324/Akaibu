5bb9e919bf65eecd0b071cc890017ce1c24b1ac8
Natalia Omiotek <natalia.omiotek15@gmail.com>
2023-11-25 20:15:14 +0100

    deleted ApplicationDbContext

--- Akaibu_Project/Startup.cs
+++ Akaibu_Project/Startup.cs
@@ -29,13 +29,10 @@ namespace Akaibu_Project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<DBAkaibuContext>(options =>
                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DBAkaibu;Trusted_Connection=True;MultipleActiveResultSets=true"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<DBAkaibuContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }

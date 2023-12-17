8b69a7109d8f511f902e3c7d791901532f9783be
Paweł Drzazga <pd301728@student.polsl.pl>
2023-12-17 14:32:15 +0100

    Poprawiono dostęp do użytkownika sesji (w trakcie).

--- Akaibu_Project/Controllers/HomeController.cs
+++ Akaibu_Project/Controllers/HomeController.cs
@@ -24,26 +24,29 @@ namespace Akaibu_Project.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly DBAkaibuContext _context;
-        private Users loggedUser;
         public HomeController(ILogger<HomeController> logger, DBAkaibuContext context)
         {
             _logger = logger;
             _context = context;
-            loggedUser = new Users();
 
+        }
+
+        private Users getLoggedUser()
+        {
+            return HttpContext.Session.GetObject<Users>("LoggedUser");
         }
 
         public IActionResult Index()
         {
             
-            return View(loggedUser);
+            return View(getLoggedUser());
             
         }
 
         public IActionResult Privacy()
         {
             
-            return View(loggedUser);
+            return View(getLoggedUser());
         }
 
         public IActionResult Add_Anime()
@@ -101,18 +104,24 @@ namespace Akaibu_Project.Controllers
 
         public IActionResult Account()
         {
-            if (User.Identity.IsAuthenticated)
+            var loggedUser = getLoggedUser();
+
+            if(loggedUser == null){
+                return View("Login");
+            }
+
+            if (loggedUser.isLogged)
             {
 
-
+                return View(loggedUser);
             }
             else
             {
-
+                return View("Login");
 
             }
 
-            return View();
+            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -156,7 +165,7 @@ namespace Akaibu_Project.Controllers
         public IActionResult Login()
         {
            
-            return View(loggedUser);
+            return View(); //zmiana
 
         }
 
@@ -169,16 +178,14 @@ namespace Akaibu_Project.Controllers
             if (u != null)
             {
                 newUser.isLogged = true;
-                loggedUser = newUser;
+                var loggedUser = newUser;
                 HttpContext.Session.SetObject("LoggedUser", loggedUser);
                 return View("Index", loggedUser);
 
             }
             else
             {
-
-
-                return View("Index", loggedUser);
+                return View("Index", getLoggedUser());
             }
         }
 
@@ -192,7 +199,7 @@ namespace Akaibu_Project.Controllers
 
         public IActionResult Register()
         {
-            return View(loggedUser);
+            return View(getLoggedUser());
         }
 
         [HttpPost]
@@ -202,26 +209,22 @@ namespace Akaibu_Project.Controllers
             var u = _context.Users
             .FirstOrDefault(u => u.Nick == newUser.Nick && u.Login == newUser.Login);
 
-            if (u!=null)
-                {
+            if (u!=null){
+                     
+                return View("Index", getLoggedUser());
                     
-                    
-                    return View("Index", loggedUser);
-                    
-                }
-                else
-                {
+            } else {
             
-                    newUser.Ranks = 1;
-                    _context.Users.Add(newUser);
-                    _context.SaveChanges();
-                    newUser.isLogged = true;
-                    loggedUser = newUser;
+                newUser.Ranks = 1;
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                newUser.isLogged = true;
+                var loggedUser = newUser;
                 HttpContext.Session.SetObject("LoggedUser", loggedUser);
                 return View("Index", loggedUser);
-                }
-            
-           
+
+            }
+  
         }
 
         public IActionResult Comments(int id)
@@ -239,6 +242,13 @@ namespace Akaibu_Project.Controllers
         [HttpPost]
         public IActionResult AddRatingAndComment(int animeId, int newRating, string newCommentText)
         {
+
+            var loggedUser = getLoggedUser();
+
+            if (loggedUser == null)
+            {
+                return View("Login");
+            }
 
             if (loggedUser.isLogged) 
             {
@@ -278,36 +288,52 @@ namespace Akaibu_Project.Controllers
 
         public IActionResult Lists()
         {
+            var loggedUser = getLoggedUser();
+
             List<Status> FinishedEmpty = new();
             List<Status> WatchedEmpty = new();
             List<Status> PlannedEmpty = new();
-            if (User.Identity.IsAuthenticated)
-            {
-                var userEmail = User.Identity.Name;
 
+            ListsModel model;
 
-                var user = _context.Users
-                .Include(u => u.Status)
-                .FirstOrDefault(u => u.Login == userEmail);
-                
-                    var viewModel = new ListsModel
-                    {
-                        Finished = user.Status.Where(s => s.StatusValue == "Finished").ToList(),
-                        Watched = user.Status.Where(s => s.StatusValue == "Watched").ToList(),
-                        Planned = user.Status.Where(s => s.StatusValue == "Planned").ToList()
-                    };
-                    return View("Lists", viewModel);
-               
-            }
-            else
+            if (loggedUser == null)
             {
-                var emptyModel = new ListsModel
+                model = new ListsModel
                 {
                     Finished = FinishedEmpty,
                     Watched = WatchedEmpty,
                     Planned = PlannedEmpty
                 };
-                return View("Lists", emptyModel);
+                return View("Lists", model);
+            }
+
+            if (loggedUser.isLogged)
+            {
+                var userEmail = loggedUser.Login;
+
+
+                var user = _context.Users
+                .Include(u => u.Status)
+                .FirstOrDefault(u => u.Login == userEmail);
+
+                model = new ListsModel
+                    {
+                        Finished = user.Status.Where(s => s.StatusValue == "Finished").ToList(),
+                        Watched = user.Status.Where(s => s.StatusValue == "Watched").ToList(),
+                        Planned = user.Status.Where(s => s.StatusValue == "Planned").ToList()
+                    };
+                    return View("Lists", model);
+               
+            }
+            else
+            {
+                model = new ListsModel
+                {
+                    Finished = FinishedEmpty,
+                    Watched = WatchedEmpty,
+                    Planned = PlannedEmpty
+                };
+                return View("Lists", model);
             }
 
             
